85efdde59ca80b747ee2f6c63bbabfdba20f5eb0
top <js.sookudom@gmail.com>
2024-06-16 15:33:04 +0700

    02 Drawing ^& Vectors

--- Simulation.js
+++ Simulation.js
@@ -8,10 +8,6 @@ class Simulation{
   }
 
   draw(){
-    ctx.beginPath();
-    ctx.rect(20,40,50,50);
-    ctx.fillStyle = "#FFA500";
-    ctx.fill();
-    ctx.closePath();
+
   }
 }
---
+++ Vector2.js
@@ -0,0 +1,44 @@
+class Vector2{
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+  }
+
+  Normalize(){
+    let length = this.Length();
+    this.x /= length;
+    this.y /= length;
+  }
+
+  Length(){
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+
+  GetNormal(){
+    return Vector2(this.y -this.x);
+  }
+
+  Dot(vec){
+    return this.x * vec.x + this.y + vec.y;
+  }
+
+  Log(){
+    console.log("Vector2 :" ,this.x ,this.y);
+  }
+
+  static Zero(){
+    return new Vector2(0,0);
+  }
+}
+
+function Add(vecA, vecB){
+  return new Vector2(vecA.x + vecB.y , vecA.y + vecB.y);
+}
+
+function Sub(vecA, vecB){
+  return new Vector2(vecA.x - vecB.y , vecA.y - vecB.y);
+}
+
+function Scale(vec,scalar){
+  return new Vector2(vec.x * scalar , vec.y * scalar);
+}
